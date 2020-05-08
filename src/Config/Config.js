12e8106214658238776e7dc718698f30484512f8
mohammedwazier <m.waziruddin@gmail.com>
2020-05-08 23:05:28 +0700

    Adding Default PORT

--- src/Config/Config.js
+++ src/Config/Config.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
 module.exports = {
-	PORT: process.env.PORT,
+	PORT: process.env.PORT || 9000,
 	SERVICE_PORT: process.env.SUB_PORT.split(',').map(data => Number(data))
 }
