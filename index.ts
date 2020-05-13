9f3dbde986b2d15d75d6e4b2cd13c09596ff18f0
mohammedwazier <m.waziruddin@gmail.com>
2020-05-13 13:44:37 +0700

    Update index Router API

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import 'module-alias/register';
 
 import express = require('express');
+import { Request, Response } from 'express';
 import cors = require('cors');
 import compression = require('compression');
 import bodyParser = require('body-parser');
@@ -10,6 +11,7 @@ import http = require('http');
 import { applyMiddleware } from "@Util/index";
 import errorHandlers from "@Middleware/ErrorHandlers";
 import { shouldCompress } from '@Middleware/Compress';
+import { MiddlewareValidation } from '@Middleware/Security';
 
 const { PORT, NAME, VERSION } = require('@Config/Config');
 const app = express();
@@ -18,18 +20,20 @@ const server = http.createServer(app);
 app.use(helmet());
 app.use(cors());
 app.use(compression({filter: shouldCompress}));
-app.use(bodyParser.urlencoded({
-	extended: true,
-	parameterLimit: 4096,
-	limit: '100mb'
-}));
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 app.disable('x-powered-by');
-applyMiddleware(errorHandlers, app);
+
+app.use(MiddlewareValidation) //Middleware Security
 
 app.use('/api', require('@Service/index'));
+app.get('/', (req: Request, res: Response) => {
+    res.send(true)
+})
+applyMiddleware(errorHandlers, app);
 
 server.listen(PORT, () => {
-    console.log(`Service Server ${NAME}\nversion '${VERSION}\nrunning on localhost:${PORT}\n`);
+    console.log(`Service Server ${NAME}\nversion ${VERSION}\nrunning on localhost:${PORT}\n`);
     console.log(`API Endpoint /api/v${VERSION.split('.')[0]}/{ nama modul }`);
 })
 
