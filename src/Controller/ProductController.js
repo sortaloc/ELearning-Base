ae6f2ccf0f0ebbb65ceba8864914b695ee746b5e
mohammedwazier <m.waziruddin@gmail.com>
2020-05-27 16:53:22 +0700

    Update

--- src/Controller/ProductController.js
+++ src/Controller/ProductController.js
@@ -302,7 +302,7 @@ class ProductController extends MainController {
                     profile.prl_username as adminuseranme,
                     produk_group.group_nama as tipegroup,
                     CONCAT('${URLIMAGE}', produk.produk_certificate) as produk_link,
-                    CONCAT('${URLIMAGE}', produk.produk_cover) as produk_link
+                    CONCAT('${URLIMAGE}', produk.produk_cover) as cover_link
                     from produk
                     JOIN profile on profile.prl_profile_id = produk.produk_id_profile
                     join produk_group on produk_group.id_group = produk.produk_id_group
