2580e4baa356a38571e04eff1d0cc26809246d2c
mohammedwazier <m.waziruddin@gmail.com>
2020-06-02 19:53:20 +0700

    Update Deployment

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -77,6 +77,25 @@ module.exports = {
       }
     },
     {
+      name: 'Payment EPresentasi',
+      script: './src/Jobs/BuyPresentasi/BuyPresentasi.js',
+      args: '',
+      instances: 1,
+      exec_mode: 'fork',
+      log_date_format: 'YYYMMDD HH',
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'development',
+        TZ: "Asia/Jakarta"
+      },
+      env_production: {
+        NODE_ENV: 'production',
+        TZ: "Asia/Jakarta"
+      }
+    },
+    {
       name: 'Deposit Expired',
       script: './src/Jobs/DepositExpire/DepositExpire.js',
       args: '',
