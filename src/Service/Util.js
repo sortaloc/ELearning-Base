7f7c2eb2fdd5016e333abf86fe38486445cd6896
mohammedwazier <m.waziruddin@gmail.com>
2020-06-05 18:29:31 +0700

    Update

--- src/Service/Util.js
+++ src/Service/Util.js
@@ -2,7 +2,7 @@ const UtilizationController = require('@Controllers/UtilizationController.js');
 
 module.exports = (router) => {
 
-    router.get('/getNumber', async (req, res) => {
+    router.get('/getUtil', async (req, res) => {
     	let response = await UtilizationController.getNumber();
     	res.send(response).status(200);
     })
