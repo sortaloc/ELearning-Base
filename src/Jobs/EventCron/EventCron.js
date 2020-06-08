8d005018248f5ba1c6550630a1c053c3d627781e
mohammedwazier <m.waziruddin@gmail.com>
2020-06-08 22:13:17 +0700

    Update

--- src/Jobs/EventCron/EventCron.js
+++ src/Jobs/EventCron/EventCron.js
@@ -47,12 +47,6 @@ const processing = async () => {
                     transaksi = transaksi[0];
                     event = event[0];
 
-                    // let pemateri = await database.profile.single({prl_profile_id: produk.produk_pemateri_id, prl_isactive: 1})
-
-                    // var globalData = {
-                    //     transaksi, pemateri, produk, akun, inbox
-                    // }
-
                     let realHarga = Number(event.event_value) /** 15000*/
                     let nexus = Number(event.event_value);
 
@@ -90,12 +84,46 @@ const processing = async () => {
 
                         if(updateProfile.rowCount > 0){
                             let cashflow = await database.cashflow.insert([jurnal1, jurnal2]);
-                            // Update Event Count
-                            // Update Inbox
-                            // Update Transaksi
-                            // Insert Outbox
                             if(cashflow.state){
-                                // 
+                                let keteranganTrx = event.nama;
+                                let updateTransaksi = await database.transaksi.updateOne(
+                                    {
+                                        trx_id: transaksi.trx_id,
+                                        trx_invoice: transaksi.trx_invoice,
+                                        trx_refid: inbox.ibx_refid
+                                    },
+                                    {
+                                        trx_saldo_after: akun.prl_saldo - realHarga,
+                                        trx_status: 'S',
+                                        trx_keterangan: keteranganTrx,
+                                        trx_updated_at: MainController.createDate(0)
+                                    })
+                                let updateInbox = await database.inbox.updateOne({ibx_refid: inbox.ibx_refid}, {ibx_status: 'S'});
+                                let Outbox = {
+                                    obx_refid: inbox.ibx_refid,
+                                    obx_id_profile: akun.prl_profile_id,
+                                    obx_interface: 'H',
+                                    obx_tipe: transaksi.trx_tipe,
+                                    obx_status: 'S',
+                                    obx_format_msg: inbox.ibx_format_msg,
+                                    obx_keterangan: `Berhasil input ke Outbox pada ${MainController.createDate(0)}`,
+                                    obx_raw_data: JSON.stringify(transaksi)
+                                }
+                                await database.outbox.insertOne(Outbox)
+                                let notifData = {
+                                  data: {
+                                    id: akun.prl_profile_id,
+                                    title: 'Event',
+                                    message: keteranganTrx,
+                                    nama_sender: 'Prexux',
+                                    menu: 'trx',
+                                    tipe: 'default',
+
+                                    send: 'user'
+                                  }
+                                }
+                                await MainController.sendNotif(notifData)
+                                ibxSucc.push(inbox.ibx_refid);
                             }else{
                                 // Cashflow Failed
                             }
@@ -105,126 +133,8 @@ const processing = async () => {
                     }else{
                         // Gagal Update jurnal 1
                     }
-
-                    // let query = `UPDATE profile SET prl_saldo_nexus = prl_saldo_nexus - ${nexus}, prl_saldo = prl_saldo - ${realHarga} WHERE prl_profile_id = '${akun.prl_profile_id}' AND prl_isactive = 1`;
-                    // let updateSaldo = await database.profile.connection.raw(query);
-
-                    // if(updateSaldo.rowCount > 0){
-                    //     let penampung = '20200604225152852310'
-                    //     let jurnal2 = {
-                    //         cf_keterangan: `Penambahan ke account Penampungan Profisiensi dengan id penampung: '${penampung}' degan pembelian Profisiensi seharga ${nexus} Nexus, dengan keuntungan sebesar ${keuntunganUserNexus} Nexus dikonversikan menjadi ${keuntunganUser} Rupiah`,
-                    //         cf_tipe: 'buy',
-                    //         cf_kredit: 0,
-                    //         cf_debet: keuntunganUser,
-                    //         cf_nominal: nexus + ' Nexus',
-                    //         cf_refid: inbox.ibx_refid,
-                    //         cf_internal_acc: penampung,
-                    //         cf_profile_id: akun.prl_profile_id,
-                    //         cf_mode: 'add_internal'
-                    //     }
-                    //     query = `UPDATE account SET acc_saldo = acc_saldo + ${keuntunganUser} WHERE acc_noakun = '${penampung}'`
-                    //     updateSaldo = await database.account.connection.raw(query);
-
-                    //     if(updateSaldo.rowCount > 0){
-
-                    //         penampung = '20200605171803213745'
-                    //         let jurnal3 = {
-                    //             cf_keterangan: `Penambahan ke account Penampungan Keuntungan Profisiensi dengan id penampung: '${penampung}' degan pembelian Profisiensi seharga ${nexus} Nexus dengan keuntungan sebesar ${feeNexus} dikonversikan menjadi ${fee} Rupiah`,
-                    //             cf_tipe: 'buy',
-                    //             cf_kredit: 0,
-                    //             cf_debet: fee,
-                    //             cf_nominal: nexus + ' Nexus',
-                    //             cf_refid: inbox.ibx_refid,
-                    //             cf_internal_acc: penampung,
-                    //             cf_profile_id: akun.prl_profile_id,
-                    //             cf_mode: 'add_profit'
-                    //         }
-                    //         query = `UPDATE account SET acc_saldo = acc_saldo + ${fee} WHERE acc_noakun = '${penampung}'`
-                    //         await database.account.connection.raw(query);
-
-                    //         let username = `PREXUX${akun.prl_nohp}${MainController.makeid(5)}`
-                    //         let password = produk.produk_kodeProduk + MainController.makeid(15)
-
-                    //         let dataRaw = {
-                    //             username: username,
-                    //             password: password,
-                    //             passwordenc: MainController.createPassword(password),
-                    //             web: produk.produk_link,
-                    //             namapemateri: pemateri.prl_nama,
-                    //             idpemateri: pemateri.prl_profile_id
-                    //         }
-
-                    //         let prfData = {
-                    //             prf_id: MainController.generateID(),
-                    //             prf_raw: JSON.stringify(dataRaw),
-                    //             prf_profile_id: akun.prl_profile_id,
-                    //             prf_trx_id: transaksi.trx_id,
-                    //             prf_username: username,
-                    //             prf_password: dataRaw.passwordenc,
-                    //             prf_refid: inbox.ibx_refid
-                    //         }
-
-                    //         let insertProfisiensi = await database.profisiensi.insertOne(prfData);
-
-                    //         if(insertProfisiensi.state){
-                    //             let cashflow = await database.cashflow.insert([jurnal1, jurnal2, jurnal3])
-                    //             dataRaw.acccess = 0;
-                    //             dataRaw.created = MainController.createDate(0);
-                    //             if(cashflow.state){
-                    //                 let keteranganTrx = `Berhasil membeli E-Profisiensi ${produk.produk_namaProduk}, E-Profisiensi dapat di download pada halaman History`;
-                    //                 let updateTransaksi = await database.transaksi.updateOne({trx_id: transaksi.trx_id, trx_invoice: transaksi.trx_invoice, trx_refid: inbox.ibx_refid}, {trx_saldo_after: akun.prl_saldo - realHarga, trx_status: 'S', trx_keterangan: keteranganTrx, trx_updated_at: MainController.createDate(0),trx_data: JSON.stringify(dataRaw)})
-                    //                 let updateInbox = await database.inbox.updateOne({ibx_refid: inbox.ibx_refid}, {ibx_status: 'S'});
-                    //                 let Outbox = {
-                    //                     obx_refid: inbox.ibx_refid,
-                    //                     obx_id_profile: akun.prl_profile_id,
-                    //                     obx_interface: 'H',
-                    //                     obx_tipe: transaksi.trx_tipe,
-                    //                     obx_status: 'S',
-                    //                     obx_format_msg: inbox.ibx_format_msg,
-                    //                     obx_keterangan: `Berhasil input ke Outbox pada ${MainController.createDate(0)}`,
-                    //                     obx_raw_data: JSON.stringify(transaksi)
-                    //                 }
-                    //                 await database.outbox.insertOne(Outbox)
-                    //                 await database.produk.connection.raw(`UPDATE produk SET produk_buy = produk_buy + 1 WHERE produk_id = '${produk.produk_id}' AND produk_kodeProduk = '${produk.produk_kodeProduk}'`)
-                    //                 let notifData = {
-                    //                   data: {
-                    //                     id: akun.prl_profile_id,
-                    //                     title: 'Berhasil membeli E-Profisiensi',
-                    //                     message: keteranganTrx,
-                    //                     nama_sender: 'Prexux',
-                    //                     menu: 'trx',
-                    //                     tipe: 'default',
-
-                    //                     send: 'user'
-                    //                   }
-                    //                 }
-                    //                 let WA = await database.setting.single({st_kode:'wa_bot'});
-                    //                 let msg = await client.messages
-                    //                 .create({
-                    //                     from: `whatsapp:+${WA.st_value}`,
-                    //                     body: `Anda telah terdaftar pada Kelas '${produk.produk_namaProduk}'\nUsername : *${username}*\nPassword : *${password}*`,
-                    //                     to: `whatsapp:+${akun.prl_nohp}`
-                    //                 })
-                    //                 console.log(msg);
-                    //                 // await twiml.message(`Anda telah terdaftar pada Kelas '${produk.produk_namaProduk}'\nUsername : *${username}*\nPassword : *${password}*`);
-                    //                 await MainController.sendNotif(notifData)
-                    //                 ibxSucc.push(inbox.ibx_refid);
-                    //             }else{
-                    //                 /*Cashflow State*/
-                    //                 /*kalau cashflow gagal, balikin saldo akun 1 dan akun 2*/
-                    //             }
-                    //         }else{
-                    //             // Failed insert profisiensi
-                    //         }
-                    //     }else{
-                    //         /*Update Akun 2*/
-                    //         /*kalau gagal pengurangan akun 2 error, balikin saldo yang pertama*/
-                    //     }
-                    // }else{
-                    //     /*Update Akun 1*/
-                    //     /*kalau gagal pengurangan akun, biarin, langsung throw error sadja*/
-                    // }
                 }
+                console.log(ibxSucc)
                 resolve(ibxSucc)
             }catch(err){
                 console.log(err);
