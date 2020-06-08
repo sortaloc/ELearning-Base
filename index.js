bafa0601d70610263abe7b0b9aefc994f09311ef
mohammedwazier <m.waziruddin@gmail.com>
2020-06-08 11:51:43 +0700

    Adding Console Status

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const { createServer } = require('http');
 const Cookie = require('cookie-parser');
 const timeout = require('connect-timeout');
 
-const { PORT, NAME, VERSION } = require('@Config/Config');
+const { PORT, NAME, VERSION, NODE_ENV } = require('@Config/Config');
 const app = express();
 const server = createServer(app);
 
@@ -37,7 +37,7 @@ app.use((req, res, next) => {
 })
 
 server.listen(PORT, () => {
-    console.log(`Service Server ${NAME}\nversion ${VERSION}\nrunning on localhost:${PORT}\n`);
+    console.log(`Service Server ${NAME}\nversion ${VERSION}\nrunning on localhost:${PORT} [${NODE_ENV}]\n`);
     console.log(`API Endpoint /api/v${VERSION.split('.')[0]}/{ nama modul }`);
 })
 
