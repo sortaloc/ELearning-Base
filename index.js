655bc1c122ff215c36bb18f322e50af3177c7b04
mohammedwazier <m.waziruddin@gmail.com>
2020-06-08 07:26:36 +0700

    Adding 404 Not Found Routes

--- index.js
+++ index.js
@@ -32,6 +32,9 @@ app.get('/', (req, res) => {
 })
 
 app.use('/files', express.static('./src/Source'));
+app.use((req, res, next) => {
+	res.status(404).send({code: 404, error: 'Route Not Found'});
+})
 
 server.listen(PORT, () => {
     console.log(`Service Server ${NAME}\nversion ${VERSION}\nrunning on localhost:${PORT}\n`);
